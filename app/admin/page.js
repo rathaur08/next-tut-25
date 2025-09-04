1222109498deb7247d6775bc24a1b407a96060d6
rathaur08jsg <sunny.rathaur@jetsetgo.in>
2025-09-04 15:30:32 +0530

    Next.js Route Groups🔥Clean URLs + Multiple Layouts for Admin & User Explained!

---
+++ app/admin/page.js
@@ -0,0 +1,13 @@
+
+const page = () => {
+  return (
+    <>
+      <div>
+        <h1>Home Admin Page</h1>
+        <p>This is the Home page of our Next.js application.</p>
+      </div>
+    </>
+  )
+}
+
+export default page
