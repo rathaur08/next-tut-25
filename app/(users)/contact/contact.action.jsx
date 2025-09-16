47e2f4a2babb05a00a15d94091ad3d17139b3264
Sunny <sunnyrathaur2444@gmail.com>
2025-09-16 16:03:33 +0530

    generateStaticParams in Next.js Explained with MyDQL DB | Convert Dynamic to Static Pages in Next JS || Server Actions Insert Contact Form Data into MySQL | Fullstack Tutorial

---
+++ app/(users)/contact/contact.action.jsx
@@ -0,0 +1,18 @@
+"use server";
+import { db } from "@/config/db";
+
+export const contactAction = async (formData) => {
+  //   const fullName = formData.get("fullName");
+  //   const email = formData.get("email");
+  //   const message = formData.get("message");
+
+  const { fullName, email, message } = Object.fromEntries(formData.entries());
+  console.log(fullName, email, message);
+
+  debugger
+
+  await db.execute(
+    `insert into contact_form (full_name, email, message) values (?, ? , ?)`,
+    [fullName, email, message]
+  );
+};
